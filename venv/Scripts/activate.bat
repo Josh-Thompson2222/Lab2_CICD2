05ce09953644581de23546ec8407aec329d67d0c
Josh <G00422756@atu.ie>
2025-09-30 19:14:17 +0100

    Added tests for update and parameterized one test.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\G00422756@atu.ie\OneDrive - Atlantic TU\Con Int Con Dev 2\Lab2Week2\week2lab2\venv
+set VIRTUAL_ENV=C:\Users\G00422756@atu.ie\OneDrive - Atlantic TU\Con Int Con Dev 2\Lab2_CICD2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
